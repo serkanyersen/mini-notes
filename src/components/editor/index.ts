2e3657125658c0ed393f99323d4e5046794790b3
Serkan Yersen <serkanyersen@gmail.com>
2016-02-06 23:35:27 -0800

    Basic editor style and copy/paste support

--- src/components/editor/index.ts
+++ src/components/editor/index.ts
@@ -1,16 +1,51 @@
-import {Component} from 'angular2/core';
+import {Component, Inject, ElementRef} from 'angular2/core';
 import {RouteParams} from 'angular2/router';
 import Notes, {Note} from '../../notes.ts';
 
+import './style.scss';
+
 @Component({
-    selector: 'editor',
-    template: `<div [(innerHTML)]="note.content" contentEditable="true"></div>`
+  selector: 'editor',
+  template: `
+      <div class="Editor-editable" [(innerHTML)]="note.content"
+          (keyup)="onChange()"
+          contentEditable="true">
+      </div>
+    `,
+  host: {
+    class: 'Editor'
+  }
 })
 export default class Editor {
-    note: Note;
+  note: Note;
+  root: HTMLElement;
 
-    constructor(private routeParams: RouteParams) {
-        const id = +routeParams.get('id');
-        this.note = Notes.get(id);
-    }
+  constructor(
+    private routeParams: RouteParams,
+    @Inject(ElementRef) elementRef: ElementRef
+  ) {
+    this.root = elementRef.nativeElement;
+    const id = +routeParams.get('id');
+    this.note = Notes.get(id);
+  }
+
+  ngAfterViewInit() {
+    this.root.querySelector('.Editor-editable').addEventListener("paste", function(e) {
+      // cancel paste
+      e.preventDefault();
+
+      // get text representation of clipboard
+      var text = (<any>e).clipboardData.getData("text/plain");
+
+      // Escape html
+      text = text.replace(/\</g, '&lt;');
+
+      // insert text manually
+      document.execCommand("insertHTML", false, text);
+    });
+  }
+
+  onChange() {
+    console.log(this.note.content);
+  }
 }
